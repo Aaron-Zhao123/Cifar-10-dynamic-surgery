ecd2c6a4edb46091abf0039e9d6ba0c30b90c8d4
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-04-24 09:53:13 +0100

    val

--- run.py
+++ run.py
@@ -119,9 +119,11 @@ while (crates['fc1'] < 3.5):
                         ]
                     _ = train.main(param)
         if (acc > 0.823):
+            crates['fc1'] = crates['fc1'] + .2
             break
         else:
             iter_cnt = iter_cnt + 1
+    crates['fc1'] = crates['fc1'] + .2
     print('accuracy summary: {}'.format(acc_list))
     print (acc)
 
