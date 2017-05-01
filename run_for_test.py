dc975c85579dfd77429b98e5b6ff480946cfa2ce
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-05-01 11:12:01 +0100

    try again

--- run_for_test.py
+++ run_for_test.py
@@ -3,33 +3,34 @@ import train
 import sys
 from shutil import copyfile
 
-def compute_file_name(pcov, pfc):
+
+def compute_file_name(p):
     name = ''
-    name += 'cov' + str(int(pcov[0] * 10))
-    name += 'cov' + str(int(pcov[1] * 10))
-    name += 'fc' + str(int(round(pfc[0] * 10)))
-    name += 'fc' + str(int(pfc[1] * 10))
-    name += 'fc' + str(int(pfc[2] * 10))
+    name += 'cov' + str(int(p['cov1'] * 10))
+    name += 'cov' + str(int(p['cov2'] * 10))
+    name += 'fc' + str(int(round(p['fc1'] * 10)))
+    name += 'fc' + str(int(p['fc2'] * 10))
+    name += 'fc' + str(int(p['fc3'] * 10))
     return name
 
 acc_list = []
 count = 0
 retrain = 0
-parent_dir = 'assets/'
 # lr = 1e-5
 lr = 1e-4
 crates = {
     'cov1': 0.,
     'cov2': 0.,
-    'fc1': 1.6,
+    'fc1': 3.0,
     'fc2': 0.,
     'fc3': 0.
 }
+parent_dir = 'assets/' + compute_file_name(crates)
 iter_cnt = 1
 retrain_cnt = 0
 roundrobin = 0
 with_biases = False
-prev_parent_dir = './assets/' + 'cr' + 'fc1v' + str(int(crates['fc1']*100)) + '/'
+f_name = compute_file_name()
 parent_dir = './assets/' + 'cr' + 'fc1v' + str(int(crates['fc1']*100)) + '/'
 iter_cnt = 0
 # TEST
